8fd127ddaeb28d22219428e448982bf9083015ba
Gearkey <gearkey@163.com>
2021-02-13 12:48:36 +0800

    playlist_set 0.2

--- 1-playlist-set/playlist_set.py
+++ 1-playlist-set/playlist_set.py
@@ -2,13 +2,14 @@ import os
 
 def main():
     # 开始
-    print('-- playlist_set v0.1 --\n')
+    print('-- playlist_set v0.2 --\n')
 
     # 指定生成的文件格式，默认 m3u
     file_format = 'm3u'
 
     # 功能选择
-    print('1，生成指定目录的播放列表\n')
+    print('1，生成指定目录的播放列表')
+    print('')
     go = input('选择需要的功能：')
 
     if go == '1': one_path(file_format)
@@ -17,9 +18,9 @@ def main():
 # 业务模块 --------------------------
 
 def one_path(file_format):
-    path = input('请输入操作路径：')
-    # path = 'D:\\temp\\t'
-    if file_format == 'm3u': print_m3u(path)
+    path = path_fix(input('请输入操作路径：'))
+    # path = 'D:\\temp\\t\\'
+    if file_format == 'm3u': print("\n文件已生成至：" + print_m3u(path))
     else: pass
 
 # 功能模块 --------------------------
@@ -36,12 +37,15 @@ def print_m3u(path):
             m3u.write(file.name + '\n')
     
     m3u.close
+    return path_out
 
 # 辅助模块 --------------------------
 
 ## 格式化路径
 def path_fix(path):
-    ### if path[]
+    if path[len(path)-1:] == '/': pass
+    elif path[len(path)-1:] == '\\': pass
+    else: path = path + '/'
     
     return path
 
