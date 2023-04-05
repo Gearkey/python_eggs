c9ca3bd2952484fb43d92fbd1590f22531ba0338
Gearkey <gearkey@163.com>
2023-04-05 18:24:22 +0800

    编辑 tieba_formatting，链接非贪婪匹配

--- 20230405_tieba_formatting/tieba_formatting.py
+++ 20230405_tieba_formatting/tieba_formatting.py
@@ -1,11 +1,11 @@
 import re
 
 def main():
-    input = open("./_note/input.md", mode="r", encoding='utf8')
-    output = open("./_note/output.md", mode="w", encoding='utf8')
+    input = open("./_note/input.md", mode="r", encoding="utf-8")
+    output = open("./_note/output.md", mode="w", encoding="utf-8")
 
     text = input.read()
-    text = re.sub(r"\[(.*)\]\((.*)\)", r"\1（\2）", text)
+    text = re.sub(r"\[(.*?)\]\((.*?)\)", r"\1（\2）", text)
     text = re.sub(r"## (.*)", r"【\1】", text)
 
     text = text.replace("\n", "\r\n")
